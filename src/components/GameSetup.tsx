a4ef48a46e7f49d2fe58d1bb7c176d44c9a844eb
Tian Zhou <tianzhou93@gmail.com>
2025-03-22 12:43:57 -0700

    Adding game setup page

---
+++ src/components/GameSetup.tsx
@@ -0,0 +1,7 @@
+function GameSetup(props: any) {
+    return (<>GameSetup
+    <button onClick={props.onStart}>Start</button>
+    </>)
+}
+
+export default GameSetup;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import Game from '../components/Game';
+import GameSetup from '../components/GameSetup';
 
 import { CharacterList } from '../lib/CharacterList';
 
 function MainPage() {
+  const [status, setStatus] = useState('new-game');
+
+  const handleGameStart = () => {
+    setStatus('in-game');
+  };
+
   return (
     <>
       <div className='page-container'>
         <div>汉字Go</div>
-        <Game list={CharacterList} />
+        {status === 'new-game' && <GameSetup onStart={handleGameStart}/>}
+        {status === 'in-game' && <Game list={CharacterList} />}
       </div>
     </>
   );
